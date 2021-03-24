66bfba4a72ae0d4fb4b217a75065fc81bf6e208d
JHurley-BD <jerome.hurley@northlink.digital>
2021-03-24 14:49:17 +0000

    init

---
+++ pages/_app.js
@@ -0,0 +1,30 @@
+import Head from 'next/head';
+
+import 'tailwindcss/tailwind.css';
+import '@ionic/react/css/core.css';
+import '@ionic/react/css/padding.css';
+import '@ionic/react/css/float-elements.css';
+import '@ionic/react/css/text-alignment.css';
+import '@ionic/react/css/text-transformation.css';
+import '@ionic/react/css/flex-utils.css';
+import '@ionic/react/css/display.css';
+
+import '../styles/global.css';
+import '../styles/variables.css';
+
+function MyApp({ Component, pageProps }) {
+  return (
+    <>
+      <Head>
+        <meta
+          name="viewport"
+          content="width=device-width, initial-scale=1.0, viewport-fit=cover"
+        ></meta>
+        <script src="https://unpkg.com/ionicons@5.2.3/dist/ionicons.js"></script>
+      </Head>
+      <Component {...pageProps} />
+    </>
+  );
+}
+
+export default MyApp;
