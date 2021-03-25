2796b6a669328f7ce6abc327083f07f11ec7a2d7
JHurley-BD <jerome.hurley@northlink.digital>
2021-03-25 13:55:45 +0000

    run on lan

---
+++ LAN-server.sh
@@ -0,0 +1,11 @@
+# Develop App on LAN so you can access it from mobile devices
+
+LAN_ADDRESS="$(ifconfig | grep -A 1 "en0" | tail -1 | cut -d ' ' -f 2)";
+PORT=3000;
+
+if [[ $LAN_ADDRESS =~ ^[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}$ ]];
+then
+   npm i && npm run prestart && next dev -p {$PORT} -H ${LAN_ADDRESS};
+else
+   npm i && npm run prestart && npm run dev;
+fi
